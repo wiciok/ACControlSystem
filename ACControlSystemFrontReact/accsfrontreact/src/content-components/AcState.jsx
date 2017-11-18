c6246cc3ef27714fa311b34509dd50d7d8128b1c
q w <wiciok1@gmail.com>
2017-11-18 19:49:37 +0100

    fetching api test code in acstate

--- ACControlSystemFrontReact/accsfrontreact/src/content-components/AcState.jsx
+++ ACControlSystemFrontReact/accsfrontreact/src/content-components/AcState.jsx
@@ -6,15 +6,36 @@ class AcState extends Component {
         this.name = "AcState";
         this.menuName = "Stan klimatyzatora";
         this.link = "/acstate";
-    }
+
+        this.rstatus = "";
+    };
+
+    fetchInfo() {
+        alert("test");
+        fetch('http://localhost:54060/api/acstate')
+            .then(function (response) {
+                if (response.status === 204) {
+                    alert(response.status);
+                    this.rstatus = "NoContent";
+                }
+            })
+            .catch(err => {
+                alert(err);
+                alert('error');
+            })
+    };
+
 
     render() {
         return (
             <div>
-                AcState
+                AcState: {this.rstatus}
+                <br />
+                <button onClick={this.fetchInfo}>
+                    test</button>
             </div>
         );
     }
-}
+};
 
 export default AcState;
