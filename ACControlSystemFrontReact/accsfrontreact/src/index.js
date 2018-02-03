98bf5d7a8fc87c746cfa6d794a57001354798fec
q w <wiciok1@gmail.com>
2018-02-03 16:20:14 +0100

    deleted unnecessary service worker

--- ACControlSystemFrontReact/accsfrontreact/src/index.js
+++ ACControlSystemFrontReact/accsfrontreact/src/index.js
@@ -2,9 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import Root from './Root';
-import registerServiceWorker from './registerServiceWorker';
 
-window["apiAddress"]="http://localhost:54060/api";
+window["apiAddress"]="http://192.168.1.110:5001/api";
+//window["apiAddress"]="http://localhost:54060/api";
 ReactDOM.render(
     <Root/>, document.getElementById('root'));
-registerServiceWorker();
